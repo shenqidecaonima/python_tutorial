0d9af5eaa29b2b9b03b1a666b4732c81574f8b2e
SHENQIDETEDDY <lim7@miamioh.edu>
2018-03-20 10:51:08 +0800

    fix bug

--- homework_01/Count.py
+++ homework_01/Count.py
@@ -4,24 +4,23 @@ import os
 number = 0#设置起始数值
 
 #输入文件名
-fileInput = input('输入计算的文件名: ')
-fileInput = './'+fileInput
+fileInput = input('输入计算的文件路径: ')
 
 def countDir(filePath):
     global number
-    print(os.getcwd())
+
     #获取计算目录中的文件信息
     dlist = os.listdir(filePath)
     #遍历所有文件，并计算目录数量
     for i in dlist:
         file = os.path.join(filePath,i)
         if os.path.isdir(file):
-            number = number + 1
             countDir(file)
+        if os.path.isfile(file):
+            number = os.stat(file).st_size + number
             print(number)
-            print(file)
 
     #输出目录数量
     return number
 
-print('子目录数量是： {}'.format(countDir(fileInput)))
+print('目录大小是： {}'.format(countDir(fileInput)))
--- homework_01/NineMultiNine.py
+++ homework_01/NineMultiNine.py
@@ -1,5 +1,6 @@
 #For loop function
 #No.1
+print('For Loop')
 for i in range(1,10):
 	for j in range(1,10):
 		if(i>=j):
@@ -26,7 +27,6 @@ while (j > 0) :
 print('*'*110)
 
 #No.3
-print('*'*110)
 for i in range(1,10):
 	for j in range(9,0,-1):
 		if(i>=j):
@@ -43,4 +43,61 @@ for i in range(9,0,-1):
 			print('{}*{}={:3}'.format(i,j,i*j), end=' ')
 		else:
 			print(end='\t')
+	print(end='\n')
+
+
+
+#While loop
+#No.1
+print('While Loop')
+i=1
+while(i<10):
+	j=1
+	while(j<10):
+		if(j<=i):
+			print('{} * {} = {}'.format(i,j,i*j),end='\t')
+		j=j+1
+	i=i+1
+	print(end='\n')
+
+print('*'*110)
+
+#No.2
+i=1
+while(i<10):
+	j=1
+	while(j<10):
+		if(j>=i):
+			print('{} * {} = {}'.format(i,j,i*j),end='\t')
+		j=j+1
+	i=i+1
+	print(end='\n')
+
+print('*'*110)
+
+#No.3
+i=0
+while(i<10):
+	j=9
+	while j>0 :
+		if(i>=j):
+			print('{}*{}={:3}'.format(i,j,i*j), end='\t')
+		else:
+			print(end='\t')
+		j = j - 1
+	i = i + 1
+	print(end='\n')
+
+print('*'*100)
+#No.4
+i=9
+while(i>0):
+	j=9
+	while j>0 :
+		if(i>=j):
+			print('{}*{}={:3}'.format(i,j,i*j), end='\t')
+		else:
+			print(end='\t')
+		j = j - 1
+	i = i - 1
 	print(end='\n')
