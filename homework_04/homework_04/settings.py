3db7148640f25d4ff16c77773eeccec2d43783b4
SHENQIDETEDDY <limoyuanamber@gmail.com>
2018-05-22 21:10:23 +0800

    fix bug

--- homework_04/homework_04/settings.py
+++ homework_04/homework_04/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'root',
-        'PASSWORD': 'Wangyizhu0923',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
